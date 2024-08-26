b607e06eee8c1c748cf093ecc3184e9b861138f9
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-26 19:34:13 +0300

    Study object literals

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 30,
+  email: 'steve@aol.com',
+  hobbies: ['music', 'sports'],
+  address: {
+    city: 'Miami',
+    state: 'FL',
+  },
+  //   ES5
+  //   getBirthYear: function () {
+  //     return 2024 - this.age;
+  //   },
+  //   ES6+
+  getBirthYear() {
+    return 2024 - this.age;
+  },
+};
+
+let val;
+
+val = person;
+
+// Get specific value
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
+
+console.log(val);
+
+const people = [
+  { name: 'John', age: 30 },
+  { name: 'Mike', age: 23 },
+  { name: 'Nancy', age: 40 },
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
