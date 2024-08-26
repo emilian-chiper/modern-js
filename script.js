2fd5fe14f341fe5f5cfa31d283fa1f82f49e478d
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-26 17:12:08 +0300

    Try out multiple console commands

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict';
 
+// 01 - Logging to console
 console.log('Hello');
+console.log(123); // nr is blue in Chrome console
+console.log(true); // also blue
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
