9be323f4fa665a3e96275ac5f640e6984c220a00
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-26 17:56:34 +0300

    Work with numbers and basic Math methods

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const num1 = 10;
+const num2 = 6;
+let val;
+
+// Arithmetic
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+val = num1 ** num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.random();
+
+// Random nr between 1 and 20
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
