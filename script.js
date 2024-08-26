0927a1c76b6e7ce280482bfe363b6f321905e95e
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-26 18:23:33 +0300

    Exercise with string template literals

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const firsName = 'John';
+const age = 30;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
+
+// Without temple strings (ES5)
+// html =
+//   '<ul><li>Name: ' +
+//   firsName +
+//   '</li><li>Age: ' +
+//   age +
+//   '</li><li>Job: ' +
+//   job +
+//   '<li>City: ' +
+//   city +
+//   '</li></ul>';
+
+// With template literals (ES6+)
+function hello() {
+  return 'hello';
+}
+
+html = `
+  <ul>
+    <li>Name: ${firsName}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
+
+document.body.innerHTML = html;
