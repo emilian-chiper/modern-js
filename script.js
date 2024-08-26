42e0a5b71acbe654c414df9a0251225f4629ba1b
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-26 19:43:36 +0300

    Exercise with date object methods

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let val;
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00 ');
+birthday = new Date('September 10 1981');
+birthday = new Date('9/10/1981');
+
+val = today;
+val = today.toISOString();
+val = today.toString();
+
+val = birthday;
+
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+// Time passed Jan 1st 1970
+val = today.getTime();
+
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1985);
+birthday.setHours(3);
+birthday.setMinutes(30);
+birthday.setSeconds(25);
+
+console.log(birthday);
