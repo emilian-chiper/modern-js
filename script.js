993b87fe819512414eada267bd79ce4121749e12
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-31 22:33:38 +0300

    Work with the window object and its methods

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 'use strict';
+
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// console.log(123);
+
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure')) {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = scrollY;
+val = scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+// window.location.href = 'http://google.com';
+
+// Reload
+// window.location.reload();
+
+// History Object
+// window.history.go(-2);
+val = window.history.length;
+
+// Navigator object
+val = window.navigator;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
