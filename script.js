d2d946fac1f0f9991d154ef0050b5a95b7f86164
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-31 22:55:23 +0300

    Work with var, let, const in global, function, and block scope

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Global scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+// Function scope
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+
+//   console.log(`Function scope: ${a}, ${b}, ${c}`);
+// }
+
+// test();
+
+// if (true) {
+//   // Block scope
+//   var a = 4; // var is NOT block-scoped but FUNCTION-scoped
+
+//   // let and const are BLOCK-scoped
+//   let b = 5;
+//   const c = 6;
+
+//   console.log(`Block scope: ${a}, ${b}, ${c}`);
+// }
+
+// again, vair is function-scoped
+for (var a = 0; a < 10; a++) {
+  console.log(`Loop ${a}`);
+}
+
+console.log(`Global scope: ${a}, ${b}, ${c}`);
