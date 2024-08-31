479f4ef967f149ae1cc94d7bffe914514af91895
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-31 21:42:53 +0300

    Work with functions
    Wrote function declarations, expressions, IIFEs, and property methods

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// FUNCTION DECLARATIONS
+// // Declaration
+// function greet() {
+//   //   console.log('Hello');
+//   return 'Hello';
+// }
+
+// Call function
+// console.log(greet());
+
+// Params
+// function greet(firstName = 'John', lastName = 'Doe') {
+//   // ES6 above
+//   // ES5 below
+//   // if (typeof firstName === 'undefined') {firstName = 'John'}
+//   return 'Hello ' + firstName + ' ' + lastName;
+// }
+
+// console.log(greet());
+
+// // FUNCTION EXPRESSIONS
+// const square = function (x = 3) {
+//   return x * x;
+// };
+
+// console.log(square());
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+// (function () {
+//   console.log('IIFE Ran...');
+// })();
+
+// (function (name) {
+//   console.log(`Hello ${name}`);
+// })('Muppet');
+
+// PROPERTY METHODS
+const todo = {
+  add: function () {
+    console.log('Add todo');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log('Delete todo');
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
